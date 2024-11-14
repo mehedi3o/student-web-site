532a561e95ec0afe69a12c88cee6d466642e22a2
mehedip1 <Promehedi30@gmail.com>
2024-11-14 11:51:25 +0600

    our school add

--- script/script.js
+++ script/script.js
@@ -28,7 +28,19 @@ switchLanguage('en');
 
 
 
-
+// faq-answer
+ // JavaScript for Accordion Toggle
+ document.querySelectorAll('.accordion-header').forEach(header => {
+    header.addEventListener('click', () => {
+        const isActive = header.classList.contains('active');
+        document.querySelectorAll('.accordion-header').forEach(h => h.classList.remove('active'));
+        document.querySelectorAll('.accordion-body').forEach(body => body.style.display = 'none');
+        if (!isActive) {
+            header.classList.add('active');
+            header.nextElementSibling.style.display = 'block';
+        }
+    });
+});
 
 
 
